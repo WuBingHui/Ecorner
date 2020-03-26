cbe4eb14f3426719309bd5fdfce1c8c496fc7586
WuBingHui <a173200488@gmail.com>
2020-03-26 15:36:14 +0800

    ViewPagerFragmentStateAdapter 導入 DiffUtil & AsyncListDiffer

--- app/src/main/java/com/anthony/ecorner/main/main/adapter/ViewPagerFragmentStateAdapter.kt
+++ app/src/main/java/com/anthony/ecorner/main/main/adapter/ViewPagerFragmentStateAdapter.kt
@@ -4,19 +4,32 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.AsyncListUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.anthony.ecorner.main.main.view.MainActivity
 
 class ViewPagerFragmentStateAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
 
+    private val DIFF_CALLBACK = object :DiffUtil.ItemCallback<Fragment>(){
+        override fun areItemsTheSame(oldItem: Fragment, newItem: Fragment): Boolean {
+           return oldItem == newItem
+        }
 
-    var fragmentList = ArrayList<Fragment>()
+        override fun areContentsTheSame(oldItem: Fragment, newItem: Fragment): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this,DIFF_CALLBACK)
 
 
     fun  setFragments(fragmentList:ArrayList<Fragment>){
 
-        this.fragmentList = fragmentList
-        notifyDataSetChanged()
+        differ.submitList(fragmentList)
+
     }
 
     fun refreshPage( Pposition: MainActivity.CurrentPages){
@@ -25,11 +38,11 @@ class ViewPagerFragmentStateAdapter(fragmentActivity: FragmentActivity) : Fragme
 
     override fun getItemCount(): Int {
 
-        return fragmentList.size
+        return differ.currentList.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return fragmentList[position]
+        return differ.currentList[position]
 
     }
 
